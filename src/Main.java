54c212c53227651c9d607bdac7b63adde81db7ce
AdamPiech <adampiech7@op.pl>
2016-09-27 12:19:59 +0200

    Zad 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
-/**
- * Created by student on 27.09.2016.
- */
+import java.util.Scanner;
+
 public class Main {
-    
+
+	public static void main(String[] args) {
+		String firstString;
+		String secondString;
+		String thirdString;
+
+		Scanner odczyt = new Scanner(System.in);
+
+		System.out.println("Podaj nazwe pliku:");
+		firstString = odczyt.nextLine();
+		System.out.println("Podaj poprawny:");
+		secondString = odczyt.nextLine();
+		System.out.println("Podaj niepoprawny:");
+		thirdString = odczyt.nextLine();
+	}
 }
+
