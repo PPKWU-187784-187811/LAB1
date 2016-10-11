55d0268dd687e57769405ffe45f97fdf166903ec
187811 <187811@edu.p.lodz.pl>
2016-10-11 10:50:56 +0200

    add method to save to file and send info about result

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +18,24 @@ public class Main {
 		secondString = odczyt.nextLine();
 		System.out.println("Podaj niepoprawny:");
 		thirdString = odczyt.nextLine();
+	}
+
+	private void saveToFile(String fileName, String secondString, String thirdString,Callback callback) {
+
+		FileOutputStream oFile = null;
+		try {
+			File file = new File(fileName);
+			file.createNewFile();
+			oFile = new FileOutputStream(file, false);
+			oFile.write("Hello world!".getBytes());
+			callback.getResult(secondString);
+			System.out.println("poprawny");
+		} catch (IOException e) {
+			System.out.println("niepoprawny");
+			callback.getResult(thirdString);
+			e.printStackTrace();
+		}
+
 	}
 }
 
